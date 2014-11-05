cbdc728375a0a722eac5a22e3a780dd9fcb37754
chieryW <chieryw@163.com>
2014-11-05 15:57:46 +0800

    Initial Commit

---
+++ SRC/t/t/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  t
+//
+//  Created by chiery on 14/11/5.
+//  Copyright (c) 2014年 qunar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SRC/t/t/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  t
+//
+//  Created by chiery on 14/11/5.
+//  Copyright (c) 2014年 qunar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
